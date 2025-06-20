c222fa0ea7b575fbae888a49b2152a56a7727290
andrejcermak <andrej.cermak.1999@gmail.com>
2025-06-20 12:44:31 +0200

    Updating post_zip

--- test/post_zip.py
+++ test/post_zip.py
@@ -7,7 +7,7 @@ import sys
 
 zip_buffer = io.BytesIO()
 
-with open('payload.json') as pl:
+with open('test/galaxy.json') as pl:
     payload = pl.read()
 
 with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zfile:
@@ -17,6 +17,10 @@ zip_buffer.seek(0)
 
 files = {'zipfile': ('files.zip', zip_buffer, 'application/zip')}
 
-response = requests.post(sys.argv[1], files=files)
+try:
+    api_path = sys.argv[1]
+    response = requests.post(api_path, files=files)
+    print('Server response:', response.text)
+except IndexError:
+    print("You did not specify API endpoint")
 
-print('Server response:', response.text)
