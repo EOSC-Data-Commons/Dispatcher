f9385e2a75bd1fa86daef077be7741c9c60487f4
andrejcermak <andrej.cermak.1999@gmail.com>
2025-07-03 14:18:05 +0200

    Fixing task registration

--- app/worker.py
+++ app/worker.py
@@ -3,9 +3,7 @@ import os
 from celery import Celery
 
 
-celery = Celery(__name__)
-celery.conf.update(accept_content=["pickle"])
+celery = Celery(__name__, include=['app.tasks'])
+celery.conf.update(accept_content = ['pickle'])
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.environ.get(
-    "CELERY_RESULT_BACKEND", "redis://localhost:6379"
-)
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
