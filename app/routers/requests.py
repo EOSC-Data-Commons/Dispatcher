78a121b5025f27fb7ec7cce8d2f810fb3ef12feb
andrejcermak <andrej.cermak.1999@gmail.com>
2025-08-06 11:42:11 +0200

    Renaming tasks to correspond with reality

--- app/routers/requests.py
+++ app/routers/requests.py
@@ -7,7 +7,7 @@ from fastapi.exceptions import HTTPException
 from app.dependencies import zipfile_parser, parse_rocrate
 import uuid
 from celery.result import AsyncResult
-from app.tasks import galaxy_from_zipfile, galaxy_from_rocrate
+from app.tasks import vre_from_zipfile, vre_from_rocrate
 import logging
 
 logger = logging.getLogger("uvicorn.error")
@@ -24,16 +24,16 @@ router = APIRouter(
 
 @router.get("/{task_id}")
 def status(token: str = Depends(oauth2_scheme), task_id: str = ""):
-    return AsyncResult(task_id).result
+    return JSONResponse({"task_id": task_id, "status": AsyncResult(task_id).status, "result": AsyncResult(task_id).result})
 
 
 @router.post("/zip_rocrate/")
 def zip_rocrate(token: str = Depends(oauth2_scheme), parsed_zipfile: (ROCrate, UploadFile) = Depends(zipfile_parser)):
-    task = galaxy_from_zipfile.apply_async(args=[parsed_zipfile, token], serializer="pickle")
+    task = vre_from_zipfile.apply_async(args=[parsed_zipfile, token], serializer="pickle")
     return JSONResponse({"task_id": task.id})
 
 
 @router.post("/metadata_rocrate/")
 def metadata_rocrate(token: str = Depends(oauth2_scheme), data: ROCrate = Depends(parse_rocrate)):
-    task = galaxy_from_rocrate.apply_async(args=[data, token], serializer="pickle")
+    task = vre_from_rocrate.apply_async(args=[data, token], serializer="pickle")
     return JSONResponse({"task_id": task.id})
--- app/tasks.py
+++ app/tasks.py
@@ -7,12 +7,18 @@ from app.internal.binder import VREBinder
 from fastapi.exceptions import HTTPException
 
 
-@celery.task(name="galaxy_from_zipfile")
-def galaxy_from_zipfile(parsed_zipfile: (ROCrate, UploadFile), token):
-    vre_handler = vre_factory(*parsed_zipfile, token=token)
-    return {"url": vre_handler.post()}
+@celery.task(name="vre_from_zipfile")
+def vre_from_zipfile(parsed_zipfile: (ROCrate, UploadFile), token):
+    try:
+        vre_handler = vre_factory(*parsed_zipfile)
+        return {"url": vre_handler.post()}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Handling request failed:\n{e}")
 
-@celery.task(name="galaxy_from_rocrate")
-def galaxy_from_rocrate(data, token):
-    vre_handler = vre_factory(crate=data, token=token)
-    return {"url": vre_handler.post()}
+@celery.task(name="vre_from_rocrate")
+def vre_from_rocrate(data, token):
+    try:
+        vre_handler = vre_factory(crate=data)
+        return {"url": vre_handler.post()}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Handling request failed:\n{e}")
