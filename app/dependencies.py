5d63c4fe83aad22c4b6f85aed2f4a8cde6e16be5
andrejcermak <andrej.cermak.1999@gmail.com>
2025-08-07 15:12:01 +0200

    Adding proper rocrate validation

--- app/dependencies.py
+++ app/dependencies.py
@@ -5,18 +5,54 @@ import io
 from fastapi import UploadFile
 from fastapi.exceptions import HTTPException
 from fastapi_oauth2.security import OAuth2AuthorizationCodeBearer
+from app.internal.vre import vre_factory, ROCrateValidationError
+from typing import Dict
 
 oauth2_scheme = OAuth2AuthorizationCodeBearer(authorizationUrl="/oauth2/login", tokenUrl="/oauth2/egi-checkin/token")
 
-def parse_rocrate(data: dict):
+def parse_rocrate(rocrate_data: Dict) -> ROCrate:
     try:
-        return ROCrate(source=data)
+        crate = ROCrate(source=rocrate_data)
+        validate_rocrate(crate)
+        return crate
     except (ValueError, KeyError) as e:
         print(data)
         raise HTTPException(
             status_code=400, detail=f"Invalid ROCrate data. Reason: {e}"
         )
 
+def validate_rocrate(crate: ROCrate):
+    check_main_entity(crate)
+    check_workflow_object(crate)
+    check_workflow_language_object(crate)
+    check_workflow_lang(crate)
+    check_vre_registered(crate)
+
+def check_main_entity(crate: ROCrate):
+    if crate.mainEntity is None:
+        raise HTTPException(
+            status_code=400, detail=f"Missing mainEntity inside ROCrate")
+
+def check_workflow_object(crate: ROCrate):
+    if type(crate.mainEntity) is str:
+        raise HTTPException(
+            status_code=400, detail=f"Missing main entiy object")
+
+def check_workflow_language_object(crate: ROCrate):
+     if type(crate.mainEntity.get("programmingLanguage")) is str:
+        raise HTTPException(
+            status_code=400, detail=f"Missing main entiy programmingLanguage object")
+
+def check_workflow_lang(crate: ROCrate):
+    if crate.mainEntity.get("programmingLanguage", {}).get("identifier") is None:
+        raise HTTPException(
+            status_code=400, detail=f"Missing programmingLanguage identifier inside ROCrate's mainEntity")
+
+def check_vre_registered(crate: ROCrate):
+    lang = crate.mainEntity.get("programmingLanguage").get("identifier")
+    if not vre_factory.is_registered(lang):
+        raise HTTPException(
+            status_code=400, detail=f"Unsupported workflow language {lang}")
 
 def parse_json_metadata(metadata: str):
     try:
@@ -40,4 +76,6 @@ def zipfile_parser(zipfile: UploadFile):
             status_code=400, detail="ro-crate-metadata.json not found in zip"
         )
     rocrate_json = parse_json_metadata(metadata)
-    return (parse_rocrate(rocrate_json), metadata)
+    rocrate = parse_rocrate(rocrate_json)
+    validate_rocrate(rocrate)
+    return (rocrate, metadata)
