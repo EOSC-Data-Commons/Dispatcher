0895ece43ee13a6384a308430f0a1decb28aed20
andrejcermak <andrej.cermak.1999@gmail.com>
2025-09-17 16:58:48 +0200

    Fixing result format

--- app/routers/requests.py
+++ app/routers/requests.py
@@ -26,7 +26,7 @@ def status(token: str = Depends(oauth2_scheme), task_id: str = ""):
         {
             "task_id": task_id,
             "status": task.status,
-            "result": str(task.result),
+            "result": str(task.result) if isinstance(task.result, Exception) else task.result,
         }
     )
 
