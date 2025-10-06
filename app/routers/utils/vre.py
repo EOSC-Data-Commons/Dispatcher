fd47312e047fe91665fa03544a107126ea45e9a4
andrejcermak <andrej.cermak.1999@gmail.com>
2025-10-06 12:45:00 +0200

    Fixed error handling in validator

--- app/routers/utils/vre.py
+++ app/routers/utils/vre.py
@@ -5,6 +5,7 @@ from fastapi import UploadFile
 from fastapi.exceptions import HTTPException
 from typing import Dict
 import io
+from app.exceptions import VREConfigurationError
 
 
 def parse_rocrate(rocrate_data: Dict) -> Dict:
@@ -12,7 +13,7 @@ def parse_rocrate(rocrate_data: Dict) -> Dict:
         crate = ROCrate(source=rocrate_data)
         validate_rocrate(crate)
         return crate.metadata.generate()
-    except (ValueError, KeyError) as e:
+    except (ValueError, KeyError, VREConfigurationError) as e:
         raise HTTPException(
             status_code=400, detail=f"Invalid ROCrate data. Reason: {e}"
         )
@@ -27,28 +28,23 @@ def validate_rocrate(crate: ROCrate):
 
 def check_main_entity(crate: ROCrate):
     if crate.mainEntity is None:
-        raise HTTPException(
-            status_code=400, detail=f"Missing mainEntity inside ROCrate"
-        )
+        raise VREConfigurationError("Missing mainEntity inside ROCrate")
 
 
 def check_workflow_object(crate: ROCrate):
     if type(crate.mainEntity) is str:
-        raise HTTPException(status_code=400, detail=f"Missing main entiy object")
+        raise VREConfigurationError("Missing main entiy object")
 
 
 def check_workflow_language_object(crate: ROCrate):
     if type(crate.mainEntity.get("programmingLanguage")) is str:
-        raise HTTPException(
-            status_code=400, detail=f"Missing main entiy programmingLanguage object"
-        )
+        raise VREConfigurationError(f"Missing main entiy programmingLanguage object")
 
 
 def check_workflow_lang(crate: ROCrate):
     if crate.mainEntity.get("programmingLanguage", {}).get("identifier") is None:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Missing programmingLanguage identifier inside ROCrate's mainEntity",
+        raise VREConfigurationError(
+            "Missing programmingLanguage identifier inside ROCrate's mainEntity"
         )
 
 
