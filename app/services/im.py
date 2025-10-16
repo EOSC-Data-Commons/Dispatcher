e822e4762cb794c1b54c8c567e5e0ac42a24cba0
Miguel Caballer Fernandez <micafer1@upv.es>
2025-10-16 14:37:07 +0200

    If 3.x_oidc_access_token use access_token (#41)
    * If 3.x_oidc_access_token use access_token
    
    * Raise error if some fields are missing
    
    * Remove unused line
    
    * Minor change

--- app/services/im.py
+++ app/services/im.py
@@ -13,7 +13,6 @@ default_im_endpoint = "https://appsgrycap.i3m.upv.es/im-dev/"
 
 class IM:
     def __init__(self, access_token: str):
-        self.deployment_type = settings.im_cloud_provider.get("deployment_type", "dev")
         auth = self._build_auth_config(access_token)
 
         if settings.im_endpoint:
@@ -26,25 +25,36 @@ class IM:
     def _build_auth_config(self, access_token: str) -> list:
         """Build authentication configuration based on deployment type."""
         auth = [{"type": "InfrastructureManager", "token": access_token}]
-        if not settings.im_cloud_provider["type"]:
+        if not settings.im_cloud_provider.get("type"):
             raise ValueError("Cloud provider type is not specified in the configuration.")
 
         if settings.im_cloud_provider["type"].lower() == "openstack":
+            for key in ["host", "username", "auth_version", "tenant"]:
+                if key not in settings.im_cloud_provider:
+                    raise ValueError(f"Missing {key} field in the OpenStack configuration")
             ost_auth = {
                 "id": "eodcostcloud",
                 "type": "OpenStack",
                 "host": settings.im_cloud_provider["host"],
                 "username": settings.im_cloud_provider["username"],
                 "auth_version": settings.im_cloud_provider["auth_version"],
-                "tenant": settings.im_cloud_provider["tenant"],
-                "password": settings.im_cloud_provider["password"]
+                "tenant": settings.im_cloud_provider["tenant"]
             }
+            if settings.im_cloud_provider["auth_version"] == "3.x_oidc_access_token":
+                ost_auth["password"] = access_token
+            else:
+                if "password" not in settings.im_cloud_provider:
+                    raise ValueError(f"Missing {key} field in the OpenStack configuration")
+                ost_auth["password"] = settings.im_cloud_provider["password"]
             if "domain" in settings.im_cloud_provider:
                 ost_auth["domain"] = settings.im_cloud_provider["domain"]
             if "region" in settings.im_cloud_provider:
                 ost_auth["service_region"] = settings.im_cloud_provider["region"]
             auth.append(ost_auth)
         elif settings.im_cloud_provider["type"].lower() == "egi":
+            for key in ["VO", "site"]:
+                if key not in settings.im_cloud_provider:
+                    raise ValueError(f"Missing {key} field in the EGI configuration")
             auth.append({
                 "id": "eodcegicloud",
                 "type": "EGI",
