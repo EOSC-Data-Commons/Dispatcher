ea2193790b50d952510077a443fa69e12d422df8
Andrej Čermák <andrej.cermak.1999@gmail.com>
2025-10-23 16:00:28 +0200

    Feature/unit testing (#35)
    * Adding refactored galaxy vre
    
    * Fixing error handling
    
    * Adding exception message to failed galaxy call
    
    * Removing redundant try except in tasks
    
    * Removing redundant serializer option
    
    * Adding more exception messages
    
    * Reformatting
    
    * Adding vre constants
    
    * Fixing result format
    
    * Adding first unit test setup
    
    * Fixing zipfile inconsistent behavior
    
    * Adding zipfiles to gitignore
    
    * Refactoring galaxy vre
    
    * Refactoring base vre
    
    * Adding new vre config exceptions
    
    * Adding dummy config.py
    
    * Extending conftest
    
    * Fixing science mesh tests
    
    * Fixing galaxy unit tests
    
    * Adding pytest.ini
    
    * quickstart
    
    * examples updated
    
    * Fixing default_im_service variable error (#33)
    
    * Adding first version of github workflow (#32)
    
    * Create python-app.yml
    
    * Fixing default_im_service variable error (#33)
    
    * Fixing pytest errors
    
    * Adding ignore depracation warning
    
    * Adding sanity test
    
    * Renaming python-app.yml
    
    * Adding dummy config.py
    
    * Adding vre language constants
    
    * Adding parser unit tests
    
    * Adding asyncio to testing suite
    
    * Polishing conftest
    
    * Adding asyncion tests to req
    
    * Fixed error handling in validator
    
    * Add ScienceMesh example documentation (#36)
    
    Co-authored-by: Rasmus Welander <rasmus.oscar.welander@cern.ch>
    
    * Feature/cloud provider info (#34)
    
    * Add cloud provider info in settings
    
    * Add region parameter
    
    * Improve log messages
    
    * Adding option to pick cloud provider method
    
    * Add support also for EGI site in conf (#30)
    
    * Add support also for EGI site in conf
    
    * Minor change
    
    * Improve code
    
    ---------
    
    Co-authored-by: Miguel Caballer <micafer1@upv.es>
    
    * examples updated
    
    * Fixing typos in main.py
    
    * Reformatting using black
    
    * Moving domain to eosc dc (#39)
    
    * Fix/demo env (#42)
    
    * Moving env to demo
    
    * Adding correct scopes
    
    * Fixing cloud provider variables
    
    * If 3.x_oidc_access_token use access_token (#41)
    
    * If 3.x_oidc_access_token use access_token
    
    * Raise error if some fields are missing
    
    * Remove unused line
    
    * Minor change
    
    ---------
    
    Co-authored-by: Miguel Caballer Fernandez <micafer1@upv.es>
    
    * Fix/ansible (#44)
    
    * Adding force checkout
    
    * Reorganizing group and host vars
    
    * Changing default host to dev3
    
    * Adding init_complete step to init compose
    
    * Reformatting
    
    ---------
    
    Co-authored-by: Aleš Křenek <ljocha@users.noreply.github.com>
    Co-authored-by: Rasmus Welander <112484950+rawe0@users.noreply.github.com>
    Co-authored-by: Rasmus Welander <rasmus.oscar.welander@cern.ch>
    Co-authored-by: Miguel Caballer <micafer1@upv.es>

--- app/celery/tasks.py
+++ app/celery/tasks.py
@@ -2,15 +2,26 @@ from .worker import celery
 from app.vres.base_vre import vre_factory
 from rocrate.rocrate import ROCrate
 from fastapi import UploadFile
+from app.exceptions import GalaxyAPIError
+from typing import Dict
+import copy
 
 
 @celery.task(name="vre_from_zipfile")
-def vre_from_zipfile(parsed_zipfile: (ROCrate, UploadFile), token):
-    vre_handler = vre_factory(*parsed_zipfile, token=token)
+def vre_from_zipfile(parsed_zipfile: (Dict, bytes), token):
+    crate = ROCrate(source=copy.deepcopy(parsed_zipfile[0]))
+    zip_file = parsed_zipfile[1]
+    vre_handler = vre_factory(crate=crate, body=zip_file, token=token)
     return {"url": vre_handler.post()}
 
 
-@celery.task(name="vre_from_rocrate")
-def vre_from_rocrate(data, token):
-    vre_handler = vre_factory(crate=data, token=token)
+@celery.task(
+    name="vre_from_rocrate",
+    autoretry_for=(GalaxyAPIError,),
+    retry_backoff=True,
+    max_retries=3,
+)
+def vre_from_rocrate(data: Dict, token):
+    crate = ROCrate(source=copy.deepcopy(data))
+    vre_handler = vre_factory(crate=crate, token=token)
     return {"url": vre_handler.post()}
