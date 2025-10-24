291801aba3b707536cca8a8451bac36097599dca
andrejcermak <andrej.cermak.1999@gmail.com>
2025-10-24 13:14:43 +0200

    Adding missing get default service

--- app/vres/scipion.py
+++ app/vres/scipion.py
@@ -6,6 +6,9 @@ logging.basicConfig(level=logging.INFO)
 
 
 class VREScipion(VRE):
+    def get_default_service(self):
+        return "https://scipion.i2pc.es/"
+
     def post(self):
         return self.svc_url
     
