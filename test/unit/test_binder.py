ee1609d14af5db8e5f28e70ed744792de9a61044
andrejcermak <andrej.cermak.1999@gmail.com>
2026-01-06 15:19:24 +0100

    Add more binder tests

--- test/unit/test_binder.py
+++ test/unit/test_binder.py
@@ -1,5 +1,6 @@
 import urllib
 from app.config import settings
+import os.path
 
 
 def test_post_happy_path(binder_vre, tmp_dir_setup):
@@ -12,3 +13,27 @@ def test_post_happy_path(binder_vre, tmp_dir_setup):
         final_url
         == f"{binder_vre.svc_url.rstrip("/")}/git/{urllib.parse.quote_plus(local_git_url)}/HEAD"
     )
+
+
+def test_post_dir_created(binder_vre, tmp_dir_setup, tmpdir):
+    request_id = "abcd1234"
+
+    binder_vre.post(request_id)
+
+    assert os.path.isdir(f"{tmpdir}/{request_id}")
+
+
+def test_post_git_repo_initialized(binder_vre, tmp_dir_setup, tmpdir):
+    request_id = "abcd1234"
+
+    binder_vre.post(request_id)
+
+    assert os.path.isdir(f"{tmpdir}/{request_id}/.git")
+
+
+def test_post_git_deamon_export_created(binder_vre, tmp_dir_setup, tmpdir):
+    request_id = "abcd1234"
+
+    binder_vre.post(request_id)
+
+    assert os.path.isfile(f"{tmpdir}/{request_id}/.git/git-daemon-export-ok")
