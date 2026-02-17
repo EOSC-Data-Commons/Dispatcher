3194883aa0ef2b7e9683b03867d4ae7e5652da68
Andrej Čermák <andrej.cermak.1999@gmail.com>
2026-02-17 14:27:20 +0100

    Fix/request id bug (#83)
    * fix request id bug
    
    * fix binder tests to use request id as class attribute

--- test/unit/test_binder.py
+++ test/unit/test_binder.py
@@ -7,10 +7,9 @@ from git import Repo
 
 
 def test_post_happy_path(binder_vre):
-    request_id = "abcd1234"
-    local_git_url = f"https://{settings.host}/git/{request_id}"
+    local_git_url = f"https://{settings.host}/git/{binder_vre._request_id}"
 
-    final_url = binder_vre.post(request_id)
+    final_url = binder_vre.post()
 
     assert (
         final_url
@@ -19,49 +18,41 @@ def test_post_happy_path(binder_vre):
 
 
 def test_post_dir_created(binder_vre, tmpdir):
-    request_id = "abcd1234"
+    binder_vre.post()
 
-    binder_vre.post(request_id)
-
-    assert os.path.isdir(f"{tmpdir}/{request_id}")
+    assert os.path.isdir(f"{tmpdir}/{binder_vre._request_id}")
 
 
 def test_post_git_repo_initialized(binder_vre, tmpdir):
-    request_id = "abcd1234"
+    binder_vre.post()
 
-    binder_vre.post(request_id)
-
-    assert os.path.isdir(f"{tmpdir}/{request_id}/.git")
+    assert os.path.isdir(f"{tmpdir}/{binder_vre._request_id}/.git")
 
 
 def test_post_git_deamon_export_created(binder_vre, tmpdir):
-    request_id = "abcd1234"
+    binder_vre.post()
 
-    binder_vre.post(request_id)
-
-    assert os.path.isfile(f"{tmpdir}/{request_id}/.git/git-daemon-export-ok")
+    assert os.path.isfile(
+        f"{tmpdir}/{binder_vre._request_id}/.git/git-daemon-export-ok"
+    )
 
 
 def test_post_git_commit(binder_vre, tmpdir):
-    request_id = "abcd1234"
-
-    binder_vre.post(request_id)
-    repo = Repo(f"{tmpdir}/{request_id}")
+    binder_vre.post()
+    repo = Repo(f"{tmpdir}/{binder_vre._request_id}")
 
     assert repo.head.commit.message == "on the fly"
 
 
 def test_post_permission_denied(binder_vre):
-    request_id = "abcd1234"
     settings.git_repos = "/"
 
     with pytest.raises(PermissionError):
-        binder_vre.post(request_id)
+        binder_vre.post()
 
 
 def test_post_not_found(binder_vre, tmpdir):
-    request_id = "abcd1234"
     settings.git_repos = f"{tmpdir}/../abc"
 
     with pytest.raises(FileNotFoundError):
-        binder_vre.post(request_id)
+        binder_vre.post()
