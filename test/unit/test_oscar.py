31292d71f26bdc52b4986e4d9c6baccc20188d21
Miguel Caballer <micafer1@upv.es>
2026-03-09 09:45:24 +0100

    Fix black

--- test/unit/test_oscar.py
+++ test/unit/test_oscar.py
@@ -58,7 +58,8 @@ fi"""
     assert mock_post.call_count == 2
 
     assert (
-        mock_post.call_args_list[0][0][0] == "https://oscar.vre.eosc-data-commons.eu/system/services"
+        mock_post.call_args_list[0][0][0]
+        == "https://oscar.vre.eosc-data-commons.eu/system/services"
     )
     fdl["script"] = script_content
     assert mock_post.call_args_list[0][1]["json"] == fdl
@@ -67,7 +68,10 @@ fi"""
         "Content-Type": "application/json",
     }
 
-    assert mock_post.call_args_list[1][0][0] == "https://oscar.vre.eosc-data-commons.eu/job/cowsay"
+    assert (
+        mock_post.call_args_list[1][0][0]
+        == "https://oscar.vre.eosc-data-commons.eu/job/cowsay"
+    )
     assert mock_post.call_args_list[1][1]["data"] == base64.b64encode(
         b"input file content"
     )
