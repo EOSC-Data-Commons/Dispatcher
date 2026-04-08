dbb06de7a1bb1895a2d728dac9d1b373af40a28e
Rasmus Welander <112484950+rawe0@users.noreply.github.com>
2026-04-08 12:52:17 +0200

    Fix sciencemesh (#81)
    * sciencemesh: update exposed protocols
    
    * sciencemesh: update default sciencemesh address
    
    * sciencemesh: fix embedded protocol and ocm address
    
    * sciencemesh: fix sciencemesh tests
    
    * sciencemesh: formatting

--- app/constants.py
+++ app/constants.py
@@ -6,8 +6,8 @@ GALAXY_PROGRAMMING_LANGUAGE = "https://galaxyproject.org/"
 BINDER_DEFAULT_SERVICE = "https://mybinder.org"
 BINDER_PROGRAMMING_LANGUAGE = "https://jupyter.org/binder/"
 
-SCIENCEMESH_DEFAULT_SERVICE = "https://cernbox.cern.ch/"
-SCIENCEMESH_PROGRAMMING_LANGUAGE = "https://cernbox.cern.ch/"
+SCIENCEMESH_DEFAULT_SERVICE = "https://qa.cernbox.cern.ch/"
+SCIENCEMESH_PROGRAMMING_LANGUAGE = "https://qa.cernbox.cern.ch/"
 
 SCIPION_DEFAULT_SERVICE = "https://scipion.i2pc.es/"
 SCIPION_PROGRAMMING_LANGUAGE = "http://scipion.i2pc.es/"
